52c0532fe4711dfdfb43e207a9f07dbe274e6bd7
copy90471 <copy90471@gmail.com>
2022-07-16 18:51:35 -0400

    Arreglando algunas url

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from app.views import get_name, home, listar, agregar
 urlpatterns = [
     path('', home, name="Home"),
     path('your-name/', get_name, name="Your Name"),
-    path('listar/', listar, name="Listar"),
-    path('agregar/', agregar, name="Agregar"),
+    path('listar/', listar, name="listar"),
+    path('agregar/', agregar, name="agregar"),
 ]
     
